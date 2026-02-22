7dbda3260e7493988951b6eba487cce1aafbe8a5
Naidu <naidugudivada768@gmail.com>
2026-02-22 05:28:37 -0600

    chore: disable elasticsearch to allow startup without aiven free tier

--- src/main/java/com/naidugudivada/ecommerce/infrastructure/config/ElasticsearchConfig.java
+++ src/main/java/com/naidugudivada/ecommerce/infrastructure/config/ElasticsearchConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.naidugudivada.ecommerce.domain.search")
+// @EnableElasticsearchRepositories(basePackages =
+// "com.naidugudivada.ecommerce.domain.search")
 public class ElasticsearchConfig {
     // Spring Boot AutoConfiguration handles the RestClient connection based on
     // the spring.elasticsearch.uris property in application.properties.
